8cd05c40f7985bd15d188c2d210f208e70d67f78
J4CK <78901234567890@mail.ru>
2020-05-07 13:52:10 +0300

    v1.0.0

---
+++ frontend/javascript/craco.config.js
@@ -0,0 +1,47 @@
+const CSS_MODULE_LOCAL_IDENT_NAME = '[local]__[hash:base64:5]';
+
+module.exports = {
+  style: {
+    modules: {
+      localIdentName: CSS_MODULE_LOCAL_IDENT_NAME,
+    },
+  },
+
+  babel: {
+    plugins: [
+      ['@babel/plugin-proposal-export-default-from'],
+
+      [
+        '@babel/plugin-proposal-decorators',
+        {
+          legacy: true,
+        },
+      ],
+
+      [
+        'babel-plugin-react-css-modules',
+        {
+          filetypes: {
+            '.scss': {
+              syntax: 'postcss-scss',
+            },
+          },
+          generateScopedName: CSS_MODULE_LOCAL_IDENT_NAME,
+          attributeNames: { class: 'className' },
+          autoResolveMultipleImports: true,
+        },
+      ],
+
+      [
+        'module-resolver',
+        {
+          alias: {
+            'components/*': './src/components/*',
+            'lib/*': './src/lib/*',
+          },
+
+        },
+      ],
+    ],
+  },
+};
